663ca14fd58972fc8b268c41ef08279086eff30d
Natalia <natalia.carrasco2101@alumnos.ubiobio.cl>
2025-07-20 22:36:23 -0400

    Modulo Clientes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { ProductListComponent } from './product/product-list/product-list.compon
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CarritoListarComponent } from './carrito/carrito-lista/carrito-listar.component';
 import { UserTableComponent } from './user-table/user-table.component';
+import { ClienteListComponent } from './cliente/cliente-list.component';
 
 export const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'product/product-list', component: ProductListComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'carrito/carrito-listar', component: CarritoListarComponent },
-  { path: 'usuarios', component: UserTableComponent}
+  { path: 'usuarios', component: UserTableComponent},
+  { path: 'clientes', component: ClienteListComponent},
 ];
